90556c97f29e85f5d5bc38aefa2863f437df3c45
Mateusz Dabrowa <143505645+mdasss@users.noreply.github.com>
2026-05-09 17:01:36 +0200

    Refactor drone detection and debug handling
    Changed the main while loop to include the direction estimation based on the sound classification

--- Drone_detection_porgram_bachelor/main.py
+++ Drone_detection_porgram_bachelor/main.py
@@ -61,12 +61,9 @@ i = 0
 while Run:
     i+=1
     samples = receiver.record() # record the data sent by the stm32
-
     # test DC offset 
     samples= samples.astype(np.float32)
     samples = samples - np.mean(samples)
-    #
-
 
     wav_conversion.convert_into_wav(samples, receive_time=receiver.start_time, duration= receiver.duration_sec) # convert the samples into a wav format
     receiver_debug.debug_export(plot_debug=True)
@@ -75,53 +72,34 @@ while Run:
     features = mfcc_extractor.extract_features(audio_file=f"debug_figures/04_05_drone_test_riktig_plassering/mic_recording{receiver.start_time}.wav") # extract features such as mfcc, delta, delta2, log_mel_spec etc
     mfcc_exporter.df_features() # export the features into a df
     # adds a lot of overhead, comment out during live testing
-    mfcc_visualizer.mel_spectrogram(receiver.start_time)
-    mfcc_visualizer.spectrogram(receiver.start_time)
-
-    # Direction
-    gcc_array = gcc_processor.process_signal(samples, mfcc_extractor.fs)
-    doa_estimator.set_gcc_array(gcc_array)
-    doa_estimator.estimate_DOA()
-    print(f"Azimuth this iteration is: {doa_estimator.estimated_azimuth_deg}")
-    print(f"Iteration: {i}")
-    # adds a lot of overhead, comment out during live testing
-    doa_visualizer.visualize_doa(doa_estimator.estimated_azimuth_deg, doa_estimator.estimated_azimuth_rad, receiver.start_time)
-
+    #mfcc_visualizer.mel_spectrogram(receiver.start_time)
+    #mfcc_visualizer.spectrogram(receiver.start_time)
+    
     # Sound classification
-    cnn_predict = cnn_model.cnn_predict(f"mic_recording{receiver.start_time}.wav") #Fix, add permanent non debug location
+    cnn_predict = cnn_model.cnn_predict(f"debug_figures/04_05_drone_test_riktig_plassering/mic_recording{receiver.start_time}.wav") #Fix, add permanent non debug location
     accumulative_predict = accumulative_model.acum_predict(features)
-
-    #Debug
+    
+    if cnn_predict == "Drone" and accumulative_predict == "Drone":
+        # Direction
+        gcc_array = gcc_processor.process_signal(samples, mfcc_extractor.fs)
+        doa_estimator.set_gcc_array(gcc_array)
+        doa_estimator.estimate_DOA()
+        print(f"Azimuth this iteration is: {doa_estimator.estimated_azimuth_deg}")
+        print(f"Iteration: {i}")
+        # adds a lot of overhead, comment out during live testing
+        #doa_visualizer.visualize_doa(doa_estimator.estimated_azimuth_deg, doa_estimator.estimated_azimuth_rad, #receiver.start_time)
+        
+    if i == 45: # allows for 45 iterations, before the program terminates
+        Run = False   
+    # debug 
     print(f"CNN prediction: {cnn_predict}")
     print(f"Accumulative prediction: {accumulative_predict}")
-
-    if i == 45:
-        Run = False
-    """
-    #Simple example of rule based drone detection
-
-    mfcc_std = features[-13:] # last mfcc std
-    mfcc_std_mean = np.mean(mfcc_std)
-
-    is_drone = mfcc_std_mean < threshold    
     
-    if i_drone:
-        angle = doa_estimator.estimated_azimuth_deg
-    else:
-        ignore
-
-    # then send the angle to the PID
-
-    """
-
-
-
     if debug_mfcc:
         mfcc_visualizer.plot_signal_time()
         mfcc_visualizer.spectrogram()
         mfcc_visualizer.mel_spectrogram()
         Run = False
-
     if debug_stm32:
         receiver_debug.debug_export(plot_debug=True)
         Run = False 
